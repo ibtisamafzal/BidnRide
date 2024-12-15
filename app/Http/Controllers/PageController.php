4e57d47a0950cf48af86fc297ea146efad0d7bb9
Muhammad Ibtisam Afzal <chaudhryibtisam2003@gmail.com>
2024-12-15 21:16:53 +0500

    App running

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,28 +1,18 @@
 <?php
+
 namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function signup()
     {
-        return view('pages.signup');
+        return view('pages.signup'); // Correct path to the signup view
+    }
+
+    public function login()
+    {
+        return view('pages.login'); // Correct path to the login view
     }
 }
-
-
-
-
-
-
-
-// namespace App\Http\Controllers;
-
-// use Illuminate\Http\Request;
-
-// class PageController extends Controller
-// {
-//     public function signup()
-//     {
-//         return view('pages.signup');
-//     }
-// }
